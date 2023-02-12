3cc6cae565751715dcc99aa2cbdb2ca9dfb8ee63
Gunny2545 <118515766+Gunny2545@users.noreply.github.com>
2023-02-12 11:21:33 +0700

    commit-message

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -9,17 +9,20 @@ app.get("/", controller.getProducts);
 app.get("/:id", controller.getProductById);
 
 // auth
-app.post("/", controller.createProduct);
+// app.post("/", controller.createProduct);
 
-app.put("/:id", controller.updateProduct);
+// app.put("/:id", controller.updateProduct);
 
-// app.post("/", auth, controller.createProduct);
+app.post("/", auth, controller.createProduct);
 
-// app.put("/:id", auth, controller.updateProduct);
+app.put("/:id", auth, controller.updateProduct);
 
 app.patch("/:id", controller.addProductReview);
 
+// auth
 app.delete("/:id", controller.deleteProductById);
+
+// app.delete("/:id", auth, controller.deleteProductById);
 
 app.get("/brand/:brand", controller.getProductsByName);
 
